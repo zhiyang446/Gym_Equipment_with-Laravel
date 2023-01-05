5abe4608a45573caa9b8e142ebd73c3b46a2aee8
zhiyang446 <50908552+zhiyang446@users.noreply.github.com>
2023-01-05 18:01:02 +0800

    initial commit

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,7 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+    <title>GoodnessGYM | @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
